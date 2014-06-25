bec63be486c6615ca7078d75ce2eb342417ad2f6
dongjiali <330767040@qq.com>
2014-06-25 17:36:09 +0800

    update new 的版本

--- MeetYou/MVC/Controller/BaseController/LeftViewController.m
+++ MeetYou/MVC/Controller/BaseController/LeftViewController.m
@@ -8,9 +8,18 @@
 
 #import "LeftViewController.h"
 #import "SliderViewController.h"
+#import "VPViewController.h"
+#import "VPImageCropperViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#define ORIGINAL_MAX_WIDTH 640.0f
 
-@interface LeftViewController ()<UITableViewDataSource,UITableViewDelegate>
+@interface LeftViewController ()<UITableViewDataSource,UITableViewDelegate, UIImagePickerControllerDelegate, UIActionSheetDelegate>
+
+@property (nonatomic, strong) UIImageView *portraitImageView;
+@property (nonatomic, strong) UIView *userInfoHeaderView;
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) UILabel *userNameLabel;
 
 @property (nonatomic, strong)NSArray *leftTableTitle;
 @end
@@ -32,8 +41,17 @@
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
 
     NSString *userName = _userInfo;
-    self.leftTableTitle = @[@"首页",@"推荐",@"活动",@"消息",@"个人",@"设置",];
+    self.leftTableTitle = @[@"推荐",@"活动",@"消息",@"设置",];
 	// Do any additional setup after loading the view.
+    
+    self.userInfoHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 220)];
+    _userInfoHeaderView.backgroundColor = [UIColor greenColor];
+    [_userInfoHeaderView addSubview:[self portraitImageView:_userInfoHeaderView]];
+    
+    self.userNameLabel = [[UILabel alloc]initWithFrame:CGRectMake(_portraitImageView.center.x - 20, _portraitImageView.frame.size.height + 60, 200, 30)];
+    _userNameLabel.text = @"橘子";
+    [_userInfoHeaderView addSubview:_userNameLabel];
+    self.tableView.tableHeaderView = _userInfoHeaderView;
 }
 
 - (void)didReceiveMemoryWarning
@@ -61,26 +79,66 @@
         case 0:
             [[SliderViewController sharedSliderController] showContentControllerWithModel:@"MainViewController"];
             break;
+//        case 1:
+//            [[SliderViewController sharedSliderController] showContentControllerWithModel:@"CommendNavigationController"];
+//            break;
         case 1:
-            [[SliderViewController sharedSliderController] showContentControllerWithModel:@"CommendNavigationController"];
-            break;
-        case 2:
             [[SliderViewController sharedSliderController] showContentControllerWithModel:@"ActivitiesNavigationController"];
             break;
-        case 3:
+        case 2:
             [[SliderViewController sharedSliderController] showContentControllerWithModel:@"MessageNavigationController"];
             break;
-        case 4:
+        case 3:
             //user root view
-            [[SliderViewController sharedSliderController] showContentControllerWithModel:@"UserInfoNavigationController"];
-            break;
-        case 5:
-            //setting root view
             [[SliderViewController sharedSliderController] showContentControllerWithModel:@"SettingNavigationController"];
             break;
         default:
             break;
     }
+}
+
+- (void)loadPortrait {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^ {
+        NSURL *portraitUrl = [NSURL URLWithString:@"http://photo.l99.com/bigger/31/1363231021567_5zu910.jpg"];
+        UIImage *protraitImg = [UIImage imageWithData:[NSData dataWithContentsOfURL:portraitUrl]];
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            self.portraitImageView.image = protraitImg;
+        });
+    });
+}
+
+- (void)editPortrait {
+    //选照片 禁止
+//    UIActionSheet *choiceSheet = [[UIActionSheet alloc] initWithTitle:nil
+//                                                             delegate:self
+//                                                    cancelButtonTitle:@"取消"
+//                                               destructiveButtonTitle:nil
+//                                                    otherButtonTitles:@"拍照", @"从相册中选取", nil];
+//    [choiceSheet showInView:self.view];
+    [[SliderViewController sharedSliderController] showContentControllerWithModel:@"UserInfoNavigationController"];
+}
+
+#pragma mark portraitImageView getter
+- (UIImageView *)portraitImageView:(UIView *)view {
+    if (!_portraitImageView) {
+        CGFloat w = 100.0f; CGFloat h = w;
+        CGFloat x = (view.frame.size.width - w) / 2 - 30;
+        CGFloat y = (view.frame.size.height - h) / 2 - 20;
+        _portraitImageView = [[UIImageView alloc] initWithFrame:CGRectMake(x, y, w, h)];
+        [_portraitImageView.layer setCornerRadius:(_portraitImageView.frame.size.height/2)];
+        [_portraitImageView.layer setMasksToBounds:YES];
+        [_portraitImageView setContentMode:UIViewContentModeScaleAspectFill];
+        [_portraitImageView setClipsToBounds:YES];
+        _portraitImageView.layer.shadowColor = [UIColor blackColor].CGColor;
+        _portraitImageView.layer.shadowOffset = CGSizeMake(4, 4);
+        _portraitImageView.layer.shadowOpacity = 0.5;
+        _portraitImageView.layer.shadowRadius = 2.0;
+        _portraitImageView.userInteractionEnabled = YES;
+        _portraitImageView.backgroundColor = [UIColor blackColor];
+        UITapGestureRecognizer *portraitTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(editPortrait)];
+        [_portraitImageView addGestureRecognizer:portraitTap];
+    }
+    return _portraitImageView;
 }
 
 @end
