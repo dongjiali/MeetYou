29a38f4c4a0f4c3e4ccafa603a436cf7851b9c45
dongjiali <330767040@qq.com>
2014-09-21 17:26:04 +0800

    update haha

--- MeetYou/MVC/Controller/Activities/ActivitiesViewController.m
+++ MeetYou/MVC/Controller/Activities/ActivitiesViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ActivitiesViewController.h"
+#import "ActivitiesSearchViewController.h"
+#import "ActivitiesTableListCell.h"
 @interface ActivitiesViewController ()
-
+@property (nonatomic,strong) NSMutableArray *activitiesArray;
+@property (nonatomic,weak) IBOutlet UITableView *tableView;
 @end
 
 @implementation ActivitiesViewController
@@ -27,6 +30,59 @@
     [super viewDidLoad];
     self.title = @"活动";
     // Do any additional setup after loading the view from its nib.
+    [self setViews];
+}
+
+- (void)setViews
+{
+    UIBarButtonItem *leftImet = [[UIBarButtonItem alloc]initWithTitle:@"左" style:UIBarButtonItemStylePlain target:self action:@selector(leftItemClick:)];
+    self.navigationItem.leftBarButtonItem = leftImet;
+    
+    UIBarButtonItem *rightImet = [[UIBarButtonItem alloc]initWithTitle:@"筛选" style:UIBarButtonItemStylePlain target:self action:@selector(rightItemClick:)];
+    self.navigationItem.rightBarButtonItem = rightImet;
+    
+}
+
+
+- (void)initialization
+{
+//    self.activitiesArray = @[@"关于",@"客服",@"更多",@"注销"];
+    [self.tableView registerClass:[ActivitiesTableListCell class] forCellReuseIdentifier:@"ActivitiesTableListCell"];// tableView
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ActivitiesTableListCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ActivitiesTableListCell" forIndexPath:indexPath];
+    return cell;
+}
+
+#pragma mark -
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+
+-(void)leftItemClick:(id)sender{
+    [[SliderViewController sharedSliderController] leftItemClick];
+}
+
+- (void)rightItemClick:(id)sender
+{
+    ActivitiesSearchViewController *viewControll = [[ActivitiesSearchViewController  alloc]init];
+    [self.navigationController pushViewController:viewControll animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
